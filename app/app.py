d62fec26f25585d668b8cf1cad23ce1d114131a1
Raghbindra Jha <raghbindrajha@Raghbindras-MacBook-Pro.local>
2025-08-05 20:04:03 +0530

    modified my flask app a bit

--- app/app.py
+++ app/app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask , render_template
+import os
 import redis
 app = Flask(__name__)
 r = redis.Redis(host='redis_container', port=6379, decode_responses=True)
 
 @app.route("/")
-def hello():
-    count = r.incr('hits')
-    return f"Hello from Raghbindra's custom Docker image! This page has been viewed {count} times."
+def index():
+    count_file = "count.txt"
+    if not os.path.exists(count_file):
+        with open(count_file, "w") as f:
+            f.write("0")
+    with open(count_file, "r+") as f:
+        count = int(f.read()) + 1
+        f.seek(0)
+        f.write(str(count))
+        f.truncate()
+    return render_template("index.html", count=count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
